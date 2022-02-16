188ada2cfa1099daf749ff872d4808c4868aa793
Takahide Sato <sato_t@cyberowl.co.jp>
2022-02-17 01:58:00 +0900

    first commit

---
+++ aliases.sh
@@ -0,0 +1,5 @@
+alias editor-up='docker-compose up sw-editor'
+alias ui-up='docker-compose up sw-ui'
+alias db-up='docker-compose up -d db'
+
+alias ui-bash='docker exec -it sw_ui sh'
---
+++ readme.md
@@ -0,0 +1,12 @@
+# setup
+
+```
+source aliases.sh
+```
+
+# container up
+```
+editor-up # swagger editor
+ui-up # swagger ui
+db-up # mysql container
+```
